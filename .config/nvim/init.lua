82eb0e94120a4ac4e0ccc7eea522e2186130a225
Lucas Russo <lerwys@gmail.com>
2026-06-25 13:11:05 -0700

    nvim: setup auto CWD

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -445,6 +445,10 @@ do
   ---@diagnostic disable-next-line: duplicate-set-field
   statusline.section_location = function() return '%2l:%-2v' end
 
+  -- set CWD as the current file
+  require('mini.misc').setup()
+  MiniMisc.setup_auto_root()
+
   -- ... and there is more!
   --  Check out: https://github.com/nvim-mini/mini.nvim
 end
